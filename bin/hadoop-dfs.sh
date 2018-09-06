6ac88a88b4eecbd2890caa8d07820b89955eab70
Andria Arisal <andria.arisal@gmail.com>
2018-09-06 13:03:55 +0700

    [ADD] cleanup. add file operations (cat, copy) on HDFS.

--- bin/hadoop-dfs.sh
+++ bin/hadoop-dfs.sh
@@ -2,29 +2,55 @@
 
 key="$1"
 case $key in
+    -cat)
+    	hdfsFile="$2"
+    	ssh ulin01 "hdfs dfs -cat $hdfsFile"
+    	;;
+    -append)
+    	localFile="$2"
+	hdfsFile="$3"
+	ssh ulin01 "hdfs dfs -appendToFile $localFile $hdfsFile"
+    	;;
     -copyFromLocal)
-    input="$2"
-    output="$3"
-    ssh ulin01 "hdfs dfs -copyFromLocal $input $output"
-    ;;
+    	localFile="$2"
+    	hdfsFile="$3"
+    	ssh ulin01 "hdfs dfs -copyFromLocal $localFile $hdfsFile"
+    	;;
     -copyToLocal)
-    input="$2"
-    output="$3"
-    ssh ulin01 "hdfs dfs -copyToLocal $input $output"
-    ;;
+    	hdfsFile="$2"
+    	localFile="$3"
+    	ssh ulin01 "hdfs dfs -copyToLocal $hdfsFile $localFile"
+    	;;
     -ls)
-    path=$2
-    ssh ulin01 "hdfs dfs -ls $path"
-    ;;
+    	hdfsPath="$2"
+    	ssh ulin01 "hdfs dfs -ls $hdfsPath"
+    	;;
+    -cp)
+	srcHdfsFile="$2"
+	dstHdfsFile="$3"
+	ssh ulin01 "hdfs dfs -cp $srcHdfsFile $dstHdfsFile"
+    -cpp)
+	srcHdfsFile="$2"
+	dstHdfsFile="$3"
+	ssh ulin01 "hdfs dfs -cp -p $srcHdfsFile $dstHdfsFile"
+    -cpf)
+	srcHdfsFile="$2"
+	dstHdfsFile="$3"
+	ssh ulin01 "hdfs dfs -cp -f $srcHdfsFile $dstHdfsFile"
     -rmr)
-    path=$2
-    ssh ulin01 "hdfs dfs -rm -r $path"
-    ;;
+    	hdfsPath="$2"
+    	ssh ulin01 "hdfs dfs -rm -r $hdfsPath"
+    	;;
     *)    # unknown option
-    echo "usage:"
-    echo "hadoop-dfs -copyFromLocal [input] [output]"
-    echo "hadoop-dfs -copyToLocal [input] [output]"
-    echo "hadoop-dfs -ls [path]"
-    echo "hadoop-dfs -rmr [path]"
+    	echo "usage:"
+    	echo "hadoop-dfs -cat [hdfsFile]"
+    	echo "hadoop-dfs -append [localFile] [hdfsFile]"
+    	echo "hadoop-dfs -copyFromLocal [localFile] [hdfsFile]"
+    	echo "hadoop-dfs -copyToLocal [hdfsFile [localFile]"
+    	echo "hadoop-dfs -ls [hdfsPath]"
+	echo "hadoop-dfs -cp [srcHdfsFile] [dstHdfsFile]"
+	echo "hadoop-dfs -cpp [srcHdfsFile] [dstHdfsFile]"
+	echo "hadoop-dfs -cpf [srcHdfsFile] [dstHdfsFile]"
+    	echo "hadoop-dfs -rmr [hdfsPath]"
     ;;
 esac
