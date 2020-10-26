f850ce3c52c4baf9be877c0affb022995d35e68a
Andria Arisal <andria.arisal@gmail.com>
2020-10-26 20:58:28 +0700

    [MOD] additional check if error.

--- bin/elsacharge.py
+++ bin/elsacharge.py
@@ -26,23 +26,24 @@ def count_charge(queue, node, slots, time):
 
 def elsa_charge(line):
     if ";E;" in line:
-        ended = line[0:line.index(";E;")]
-        user  = line[line.index("user=")+5:line.index(" group")]
-        queue = line[line.index("queue=")+6:line.index(" ctime")]
-        node  = line[line.index("unique_node_count")+18:line.index(" end")]
-        slots = line[line.index("total_execution_slots")+22:line.index(" unique_node_count")]
-        time  = line[line.index("resources_used.walltime")+24:line.index("resources_used.walltime")+32]
+        if not "Error_Path" in line:
+            ended = line[0:line.index(";E;")]
+            user  = line[line.index("user=")+5:line.index(" group")]
+            queue = line[line.index("queue=")+6:line.index(" ctime")]
+            node  = line[line.index("unique_node_count")+18:line.index(" end")]
+            slots = line[line.index("total_execution_slots")+22:line.index(" unique_node_count")]
+            time  = line[line.index("resources_used.walltime")+24:line.index("resources_used.walltime")+32]
 
-        # return user + ';' + queue + ';' + slots + ';' + time + ';' + str(count_charge(queue, node, slots, get_sec(time)))
-        elsaC = dict()
-        elsaC['user'] = user
-        elsaC['queue'] = queue
-        elsaC['slots'] = int(slots)
-        elsaC['time'] = time
-        elsaC['ended'] = ended
-        elsaC['points'] = round(count_charge(queue, node, slots, get_sec(time)), 2)
-        elsaC_json = json.dumps(elsaC)
-        return  elsaC_json
+            # return user + ';' + queue + ';' + slots + ';' + time + ';' + str(count_charge(queue, node, slots, get_sec(time)))
+            elsaC = dict()
+            elsaC['user'] = user
+            elsaC['queue'] = queue
+            elsaC['slots'] = int(slots)
+            elsaC['time'] = time
+            elsaC['ended'] = ended
+            elsaC['points'] = round(count_charge(queue, node, slots, get_sec(time)), 2)
+            elsaC_json = json.dumps(elsaC)
+            return  elsaC_json
     else:
         return ""
 
